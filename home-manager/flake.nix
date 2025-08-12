b66dc75e7fa3b5cbaf11cb77528bafa664045dc4
NeoUmbreon <neohasmail30@gmail.com>
2025-08-12 05:23:34 -0400

    Add comments back to home-manager

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,14 +2,20 @@
   description = "Home Manager configuration of dawn";
 
   inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    # Unstable
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # 25.05
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     home-manager = {
+      # Unstable
+      #url = "github:nix-community/home-manager";
+      # 25.05
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # ADD THIS:
     nyx.url = "github:Peritia-System/Nyx-Tools";
   };
 
@@ -21,9 +27,14 @@
     {
       homeConfigurations."dawn" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
         modules = [ ./home.nix ];
 
-        # So `home.nix` can use `inputs.nyx`
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        # So home.nix can use inputs.nyx
         extraSpecialArgs = { inherit nyx; };
       };
     };
