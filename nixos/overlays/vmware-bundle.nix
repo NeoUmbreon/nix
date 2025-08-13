b0691f8c485f1248d2f1089058db67a1a000e117
NeoUmbreon <neohasmail30@gmail.com>
2025-08-13 09:06:16 -0400

    Removed vmware overlay

--- nixos/overlays/vmware-bundle.nix
+++
@@ -1,9 +0,0 @@
-self: super: {
-  vmware-workstation = super.vmware-workstation.overrideAttrs (old: {
-    src = super.fetchurl {
-      url = "file:///home/dawn/flakes/nixos/overlays/VMware-Workstation-Full-17.6.3-24583834.x86_64.bundle";
-      sha256 = "1q4f8wjv23dc28fqvdpf00y7g6myh5mapx491dnlrvcdf8bmjmvr";
-    };
-  });
-}
-
