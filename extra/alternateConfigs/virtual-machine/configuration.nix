5ef5811f011930662cc9fa155e23b1e7551cc620
neoumbreon <neohasmail30@gmail.com>
2025-08-16 19:49:36 -0400

    Add extra/alternateConfigs for virtual machine

---
+++ extra/alternateConfigs/virtual-machine/configuration.nix
@@ -0,0 +1,314 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  services.udev.extraRules = ''
+    #xx3dsfml
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="601e", MODE="0666"
+
+    #GameCube Controller Adapter
+    SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337",     TAG+="uaccess"
+
+    #Wiimotes or DolphinBar
+    SUBSYSTEM=="hidraw*", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0306", TAG+="uaccess"
+    SUBSYSTEM=="hidraw*", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0330", TAG+="uaccess"
+  '';
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+  #time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.flatpak.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+    ];
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    #wireplumber.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dawn = {
+    isNormalUser = true;
+    description = "dawn";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd"];
+    packages = with pkgs; [
+      # core system packages
+      wine
+      wine64
+      git
+      neovim
+      gcc
+      kdePackages.kate
+      #nil
+      marksman
+      kdePackages.markdownpart
+      #sfml_2
+
+      # Bash prompt
+      #gitstatus
+
+      # LazyVim (this doesn't work)
+      #vimPlugins.LazyVim
+
+      # Gaming
+      protonplus
+      joystickwake
+      umu-launcher
+      prismlauncher
+      dolphin-emu
+
+      # Media and Audio
+      easyeffects
+      gnomeExtensions.easyeffects-preset-selector
+      alsa-scarlett-gui
+      pwvucontrol
+      kdePackages.kdenlive
+      #qpwgraph
+      #pavucontrol
+
+      # Graphics and Display
+      nwg-look
+
+      # KDE Packages
+      kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+      kdePackages.kcalc # Calculator
+      kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+      kdePackages.kcolorchooser # A small utility to select a color
+      kdePackages.kolourpaint # Easy-to-use paint program
+      kdePackages.ksystemlog # KDE SystemLog Application
+      kdePackages.sddm-kcm # Configuration module for SDDM
+      kdiff3 # Compares and merges 2 or 3 files or directories
+      kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
+      hardinfo2 # System information and benchmarks for Linux systems
+      #kdePackages.kmix
+
+      wayland-utils # Wayland utilities
+      wl-clipboard # Command-line copy/paste utilities for Wayland
+
+      # Utilities
+      #neofetch
+      fastfetch
+      fsearch
+      xed-editor
+      qdirstat
+      #discord
+      vscode
+      github-desktop
+      element-desktop
+      parabolic
+      google-chrome
+      #putty
+      tree
+      ncdu
+
+      # Office
+      thunderbird
+      nextcloud-client
+      zoom-us
+    ];
+  };
+
+  # QEMU/KVM
+  #programs.virt-manager.enable = true;
+
+  #users.groups.libvirtd.members = ["dawn"];
+
+  #virtualisation.libvirtd.enable = true;
+
+  #virtualisation.spiceUSBRedirection.enable = true;
+
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+
+
+  # OBS Studio with plugins
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-composite-blur
+      obs-shaderfilter
+      obs-scale-to-sound
+      obs-move-transition
+      obs-gradient-source
+      obs-replay-source
+      obs-source-clone
+      obs-3d-effect
+      obs-livesplit-one
+      waveform
+      obs-gstreamer
+      obs-vaapi
+      obs-vkcapture
+    ];
+  };
+
+  #libfuse2 apps
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+
+
+   # Nix ld
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+    fuse
+    icu
+    icu.dev
+    onnxruntime
+    sdl3
+    freetype
+    harfbuzz
+    fwupd
+  ];
+
+ # Hardware and gaming
+  hardware.steam-hardware.enable = true;
+  hardware.graphics.enable32Bit = true;
+  hardware.xone.enable = true;
+  hardware.xpadneo.enable = true;
+  hardware.opentabletdriver.enable = true;
+  programs.gamemode.enable = true;
+  programs.steam.enable = true;
+  programs.gamescope.enable = true;
+  programs.gamescope.capSysNice = true;
+
+  # Noisetorch
+  programs.noisetorch.enable = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Bash prompt gitstatus
+  #programs.bash.promptInit = "source $(gitstatus-share)/gitstatus.prompt.sh";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.auto-optimise-store = true;
+
+  fonts.enableDefaultPackages = true;
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.hack
+  ];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
