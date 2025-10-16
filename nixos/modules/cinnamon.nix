0a7c3ff48a5f889dcd106fa747bb24002751b81e
neoumbreon <neohasmail30@gmail.com>
2025-10-16 13:27:39 -0400

    Fixes for cinnamon.nix

--- nixos/modules/cinnamon.nix
+++ nixos/modules/cinnamon.nix
@@ -10,8 +10,6 @@
     # You can disable this if you're only using the Wayland session.
     services.xserver = {
       enable = true;
-      # Enable touchpad support (enabled default in most desktopManager).
-      libinput.enable = true;
       # Configure keymap in X11
       xkb = {
         layout = "us";
@@ -25,7 +23,18 @@
       };
       #displayManager.defaultSession = "cinnamon";
     };
+    # Enable touchpad support (enabled default in most desktopManager).
+    services.libinput.enable = true;
+
     # Enable cinnamon apps
     services.cinnamon.apps.enable = true;
+    # Exclude packages from cinnamon apps
+    #environment.cinnamon.excludePackages = []
+
+    # Fix for QT app sizes (OBS, Kdenlive, etc.)
+    environment.sessionVariables = {
+      QT_AUTO_SCREEN_SCALE_FACTOR = "0";
+      XCURSOR_SIZE = "48";
+    };
   };
 }
